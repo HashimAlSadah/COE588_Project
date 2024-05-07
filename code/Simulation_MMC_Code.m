20ea53c51c4c429ef3db41d9ef08596ea4c19930
Wahab <abdwhbgh@gmail.com>
2024-05-07 17:24:22 +0300

    added n_trials in code and a bit of latex

--- code/Simulation_MMC_Code.m
+++ code/Simulation_MMC_Code.m
@@ -4,8 +4,14 @@ lambda = 16;
 mue = 10;
 c = 2;
 rho = lambda / (c * mue);
+n_trials = 5;
+sim_E_W = 0;
+sim_E_T = 0;
+sim_E_N = 0;
+sim_E_Nq = 0;
+sim_rho = 0;
 
-
+for i = 1:n_trials 
 % Initialize lists
 AT = cumsum(exprnd((1/lambda)*ones(1,numCustomers)));
 ST = exprnd((1/mue)*ones(1,numCustomers));
@@ -17,18 +23,24 @@ n_runs = 1;
 [DT, startList, serviceTime] = simulation_loop(AT, ST, c);
 WT = startList - AT;
 TT = WT + ST;
-sim_E_W = mean(WT);
-sim_E_T = mean(TT);
+sim_E_W = sim_E_W + mean(WT);
+sim_E_T = sim_E_T + mean(TT);
 probWait = sum( WT > 0) / length(WT);
 
 %-------E[N], E[Nq]-------
 [areaN, areaNq] = PMF_and_track_N(AT, DT, c);
 maxDT = max(DT);
-sim_E_N = areaN / maxDT;
-sim_E_Nq = areaNq / maxDT;
-sim_rho = mean(serviceTime / maxDT);
+sim_E_N = + sim_E_N + areaN / maxDT;
+sim_E_Nq = sim_E_Nq + areaNq / maxDT;
+sim_rho =sim_rho + mean(serviceTime / maxDT);
 
 
+end 
+sim_E_W = sim_E_W / n_trials;
+sim_E_T = sim_E_T / n_trials;
+sim_E_N = sim_E_N / n_trials;
+sim_E_Nq = sim_E_Nq / n_trials;
+sim_rho = sim_rho / n_trials;
 
 %-------theoretical results------
 [theo_PMF, theo_E_N, theo_E_Nq, theo_E_T, theo_E_W] = ...
